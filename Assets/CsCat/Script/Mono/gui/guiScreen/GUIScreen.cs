9359b508a49f22bd916f4fc4130de67e133e0ed9
uiopsczc <uiop8179>
2021-06-12 00:52:25 +0800

    8

--- Assets/CsCat/Script/Mono/gui/guiScreen/GUIScreen.cs
+++ Assets/CsCat/Script/Mono/gui/guiScreen/GUIScreen.cs
@@ -14,25 +14,25 @@ namespace CsCat
     void OnGUI()
     {
       action?.Invoke();
-      using (new GUILayoutBeginVerticalScope())
-      {
-        using (new GUIMatrixScope(Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1.5f, 1.5f, 1))))
-        {
-          is_show_log = GUILayout.Toggle(is_show_log, "显示日志");
-          if (is_show_log)
-            is_lockTo_bottom = GUILayout.Toggle(is_lockTo_bottom, "锁屏");
-        }
-
-        if (is_show_log)
-        {
-          if (is_lockTo_bottom)
-            scrollPosition = new Vector2(scrollPosition.x, float.MaxValue);
-          using (new GUILayoutBeginScrollViewScope(ref scrollPosition))
-          {
-            LogCat.Flush_GUI();
-          }
-        }
-      }
+//      using (new GUILayoutBeginVerticalScope())
+//      {
+//        using (new GUIMatrixScope(Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1.5f, 1.5f, 1))))
+//        {
+//          is_show_log = GUILayout.Toggle(is_show_log, "显示日志");
+//          if (is_show_log)
+//            is_lockTo_bottom = GUILayout.Toggle(is_lockTo_bottom, "锁屏");
+//        }
+//
+//        if (is_show_log)
+//        {
+//          if (is_lockTo_bottom)
+//            scrollPosition = new Vector2(scrollPosition.x, float.MaxValue);
+//          using (new GUILayoutBeginScrollViewScope(ref scrollPosition))
+//          {
+//            LogCat.Flush_GUI();
+//          }
+//        }
+//      }
     }
 
     void Awake()
